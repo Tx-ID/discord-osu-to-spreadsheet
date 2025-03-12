f6b97486c2eaadf708efa2e536ab31e6710afc8a
Tix <67917789+Tx-ID@users.noreply.github.com>
2025-03-13 00:37:14 +0700

    key

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ export default () => ({
     sheets: {
         email: process.env.SHEETS_ACCOUNT_EMAIL,
         id: process.env.SHEETS_ACCOUNT_ID,
-        key: process.env.SHEETS_ACCOUNT_KEY,
+        key: process.env.SHEETS_ACCOUNT_KEY?.split(String.raw`\n`).join('\n'),
     },
     discord: {
         application_id: process.env.DISCORD_APPLICATION_ID,
